4b9f7c6dd8b6a3bddfe2e505fc48a399606cf896
Gustavo <gustavogarcia56336@gmail.com>
2025-09-04 15:31:00 -0300

    feat: functional with docker compose in root

--- pix-commons/src/main/java/itau/pix/commons/config/DatabaseConstants.java
+++ pix-commons/src/main/java/itau/pix/commons/config/DatabaseConstants.java
@@ -7,5 +7,5 @@ public class DatabaseConstants {
     public static final String DATABASE_NAME = "itau_challenge";
     public static final String PIX_COLLECTION = "chaves_pix";
     public static final String TRANSACTION_COLLECTION = "transacoes_pix";
-    public static final String CONNECTION_STRING = "mongodb://localhost:27017/itau_challenge";
+    public static final String CONNECTION_STRING = "mongodb://mongo-local:27017/itau_challenge";
 }
--- pix-gateway/src/main/java/itau/gateway/queue/infrastructure/config/MongoConfig.java
+++ pix-gateway/src/main/java/itau/gateway/queue/infrastructure/config/MongoConfig.java
@@ -1,9 +1,12 @@
 package itau.gateway.queue.infrastructure.config;
 
+import org.bson.UuidRepresentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 import itau.pix.commons.config.DatabaseConstants;
@@ -12,10 +15,17 @@ import itau.pix.commons.config.DatabaseConstants;
 public class MongoConfig {
 
     @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(
-                MongoClients.create(DatabaseConstants.CONNECTION_STRING),
-                DatabaseConstants.DATABASE_NAME
+    public MongoClient mongoClient() {
+        return MongoClients.create(
+                MongoClientSettings.builder()
+                        .applyConnectionString(new com.mongodb.ConnectionString(DatabaseConstants.CONNECTION_STRING))
+                        .uuidRepresentation(UuidRepresentation.STANDARD) // 🔑 aqui define o padrão
+                        .build()
         );
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, DatabaseConstants.DATABASE_NAME);
     }
 }
